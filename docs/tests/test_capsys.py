35173a96c62da31ddb464f22e0caa50e73f27958
shinseitaro <shinsei.all@gmail.com>
2023-07-11 19:58:17 +0900

    テスト修正

--- docs/tests/test_capsys.py
+++ docs/tests/test_capsys.py
@@ -35,25 +35,25 @@ def test_version_v2():
 #  event_id="266898"で取得できるデータの "results_returned" キーに入っているデータは1であることをprintして
 # 出力をキャプチャして、それが１であることをテストして下さい
 
-def test_results_returned(capsys):
+def test_capsys_1(capsys):
     data = connpass_client.ConnpassClient().get(event_id="266898")
     print(data["results_returned"])
     assert capsys.readouterr().out.strip() == "1"
 
 #  connpass_client には post メソッドが無いので、呼び出そうとすると AttributeError が発生します。
 # 教科書の 2.6 想定される例外をテストする を参考に期待するエラーが発生するテストを書いて下さい。
-def test_notexist_method():
+def test_capsys_2():
     with pytest.raises(AttributeError):
         connpass_client.ConnpassClient.post()
 
 # 存在しない event-id (例 8888888888) を渡すと、
 # {'events': [],\n 'results_available': 0,\n 'results_returned': 0,\n 'results_start': 1} が返るテストを書いて下さい
 
-def test_notexist_eventid():
-    output = subprocess.run(["python", "-m", "connpass_client", "--event-id", "88888888"],
-               capture_output=True, text=True)
-    expected = """{'events': [],\n 'results_available': 0,\n 'results_returned': 0,\n 'results_start': 1}"""
-    assert output.stdout.strip() == expected
+def test_capsys_3():
+    runner = CliRunner()
+    result = runner.invoke(connpass_client.app, [ "--event-id", "8888888888"])
+    output = result.stdout.rstrip()
+    assert output == """{'events': [],\n 'results_available': 0,\n 'results_returned': 0,\n 'results_start': 1}"""
     
 
 
@@ -61,11 +61,14 @@ def test_notexist_eventid():
 # 以下のエラーメッセージが出力されることを subprocess.run を使ってテストを書いて下さい
 # """Usage: python -m connpass_client [OPTIONS]\nTry 'python -m connpass_client --help' for help.\n\nError: No such option: --taro Did you mean --start?"""
 
-def test_notexist_option():
+def test_capsys_4():
+
     output = subprocess.run(
         ["python", "-m", "connpass_client", "--taro"],
         capture_output=True, 
         text=True
     )
-    assert output.stderr.strip() == """Usage: python -m connpass_client [OPTIONS]\nTry 'python -m connpass_client --help' for help.\n\nError: No such option: --taro Did you mean --start?"""
+    assert output.stderr.strip() == """Usage: python -m connpass_client [OPTIONS]
+Try 'python -m connpass_client --help' for help.
 
+Error: No such option: --taro Did you mean --start?"""
